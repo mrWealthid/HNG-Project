7bdfba376f61c3ee7c89984a0eadae78ecdae66e
Wealth Iduwe <wealthiduwe@gmail.com>
2021-07-03 14:03:39 +0100

    first commit

---
+++ project.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// collapsable NavBar
+let headerLinks = document.querySelector(".header-links");
+let harmburger = document.querySelector("#icons");
+let overlay = document.querySelector(".overlay");
+
+//when an option on the overlay, headerlink and harmburger is clicked- it is hidden the side bar is hidden
+
+const Toggler = function () {
+  headerLinks.classList.contains("header-links") &&
+    headerLinks.classList.toggle("show");
+  overlay.classList.toggle("overlay-filter");
+};
+
+harmburger.addEventListener("click", Toggler);
+
+headerLinks.addEventListener("click", Toggler);
+
+overlay.addEventListener("click", Toggler);
