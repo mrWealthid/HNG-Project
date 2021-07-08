015c4db5e3fbe0239559049ea77a9af86337af6d
Wealth Iduwe <wealthiduwe@gmail.com>
2021-07-08 13:36:18 +0100

    final modifications

--- Js/project.js
+++ Js/project.js
@@ -23,3 +23,25 @@ overlay.addEventListener("click", Toggler);
 document.addEventListener("keydown", (e) => {
   e.key === "Escape" && Toggler();
 });
+
+// Reveal sections
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
